3eabbcb9cf537264e03fecee9d8095e162e18d79
T6nu <74606960+T6nuk@users.noreply.github.com>
2025-04-01 18:46:06 +0000

    Add button to clear grid, input to pick color and functionality

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-// create a 16x16 grid
+//
 const container = document.querySelector("#container");
-const btn = document.querySelector("button");
-
-btn.addEventListener("click", () => {
-  container.replaceChildren();
-  let newRows = prompt("How many rows on your new grid?");
-  if (newRows > 64) return alert("pick a smaller number, max 64");
-  if (isNaN(newRows)) return alert("not a number, please write a number");
-  console.log(newRows);
-
-  createGrid(newRows);
-});
+// buttons on top
+const newGridBtn = document.querySelector("#button-new-grid");
+const clearGridBtn = document.querySelector("#button-clear-grid");
+// color selector
+const colorPicker = document.querySelector("#color-select");
+// default color
+let color = "black";
 
 function createGrid(rowsN) {
   let rows = rowsN;
@@ -27,13 +23,39 @@ function createGrid(rowsN) {
       columnItem = document.querySelector(".column-item");
       column.style.display = "flex";
       column.style.flex = "1";
-      column.style.backgroundColor = "pink";
+      column.style.border = "1px dotted black";
       row.appendChild(column);
 
-      //   column.addEventListener("mouseover", () => {
-      //     column.style.backgroundColor = "red";
-      //   });
+      column.addEventListener("mouseover", () => {
+        paintSquare(column, color);
+      });
     }
     container.appendChild(row);
   }
 }
+
+createGrid(32);
+
+function paintSquare(tile, color) {
+  tile.style.backgroundColor = color;
+}
+
+colorPicker.addEventListener("input", () => {
+  color = colorPicker.value;
+});
+
+newGridBtn.addEventListener("click", () => {
+  container.replaceChildren();
+  let newRows = prompt("How many rows on your new grid?");
+  if (newRows > 64) return alert("pick a smaller number, max 64");
+  if (isNaN(newRows)) return alert("not a number, please write a number");
+
+  createGrid(newRows);
+});
+
+clearGridBtn.addEventListener("click", (event) => {
+  let tileList = document.querySelectorAll(".column-item");
+  tileList.forEach((node) => {
+    paintSquare(node, "antiquewhite");
+  });
+});
